4804cdb61af829603f7f2b7e87993e1c7ac6b3ba
Shri nivas <shri.nivas@troology.com>
2022-04-29 15:51:36 +0530

    git push

--- sidm(backend)/controllers/register.js
+++ sidm(backend)/controllers/register.js
@@ -1,5 +1,15 @@
 const RegistrationForm = require("../models/registrationForm");
 const fs = require("fs");
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'sidm.award@gmail.com',
+    pass: 'sidmaward123@'
+  }
+});
+
 
 exports.postRegistrationForm = (req, res, next) => {
 
@@ -81,6 +91,19 @@ exports.postRegistrationForm = (req, res, next) => {
   form
     .save()
     .then((result) => {
+      var mailOptions = {
+        from: 'sidm.award@gmail.com',
+        to: result.email,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
       res.status(200).json('successfully sumbit');
     })
     .catch((err) => {
