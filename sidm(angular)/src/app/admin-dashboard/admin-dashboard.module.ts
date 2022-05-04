cc66bd0dfdde6f3579b9f83dd3cd37172a91e1ef
shrinivas <shrinivas@foreignadmits.com>
2022-05-04 19:34:12 +0530

    git push

--- sidm(angular)/src/app/admin-dashboard/admin-dashboard.module.ts
+++ sidm(angular)/src/app/admin-dashboard/admin-dashboard.module.ts
@@ -14,6 +14,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
+import { MatTableExporterModule } from 'mat-table-exporter';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatFormFieldModule,
     MatMenuModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableExporterModule
   ]
 })
 export class AdminDashboardModule { }
