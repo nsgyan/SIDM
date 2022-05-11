91f176a12ff761316bf7b7fb977e05c3dff673d1
Shri nivas <shri.nivas@troology.com>
2022-05-11 16:43:47 +0530

    new changes

--- sidm(angular)/src/app/dashboard/memberdashboard/memberdashboard-routing.module.ts
+++ sidm(angular)/src/app/dashboard/memberdashboard/memberdashboard-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApplyNewCategoryComponent } from './apply-new-category/apply-new-category.component';
 import { MemberDashboardComponent } from './member-dashboard/member-dashboard.component';
+import { QuestionnaireFormComponent } from './questionnaire-form/questionnaire-form.component';
 
 
 const routes: Routes = [
   { path: 'view/:id', component: MemberDashboardComponent },
   { path: 'applyNew', component: ApplyNewCategoryComponent },
+  { path: 'applyNew', component: ApplyNewCategoryComponent },
+  { path: 'questionnaire', component:QuestionnaireFormComponent },
 ];
 
 @NgModule({
