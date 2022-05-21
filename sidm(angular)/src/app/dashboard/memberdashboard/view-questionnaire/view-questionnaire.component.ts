9a462745330a733d75d44c7d8b6109edeed77298
Shri nivas <shri.nivas@troology.com>
2022-05-21 16:47:32 +0530

    git push

--- sidm(angular)/src/app/dashboard/memberdashboard/view-questionnaire/view-questionnaire.component.ts
+++ sidm(angular)/src/app/dashboard/memberdashboard/view-questionnaire/view-questionnaire.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HttpService } from 'src/app/shared/services/http.service';
+import { ModelComponent } from 'src/app/shared/services/model/model.component';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-view-questionnaire',
@@ -17,13 +20,19 @@ export class ViewQuestionnaireComponent implements OnInit {
     private httpService: HttpService,
     private fb:FormBuilder,
     private toast: ToastrService,
-    private route: ActivatedRoute,) { 
+    private route: ActivatedRoute,
+    public dialog: MatDialog) { 
     this.id = this.route.snapshot.paramMap.get('id')
     this.questionnaireForm=this.fb.group({
       aissment: this.fb.array([]) 
     })
-    this.httpService.getQuestionnaireAissment(this.id).subscribe(data=>{
+    this.httpService.getQuestionnaireAissment(this.id).subscribe((data:any)=>{
       console.log(data);
+      data[0].questionAns.map((item:any)=>{
+        if(item.uploadDocuments){
+          item.uploadDocuments = environment.download + item.uploadDocuments
+        }
+      })
       this.aissmentdata=data
       
     })
@@ -34,5 +43,11 @@ export class ViewQuestionnaireComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  openModel(data:any){
+    const dialogRef = this.dialog.open(ModelComponent, {
+      width: '500px',
+      data: {data: data,type:'ViewQuestionnaire'},
+    });
+    
+  }
 }
