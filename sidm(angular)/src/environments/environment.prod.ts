7b3df18633fe1378ca0fc7ddc8c744511b7721f3
CXZneha1211 <anamika.kushwaha@troology.com>
2022-05-24 12:59:06 +0530

    stepper changes

--- sidm(angular)/src/environments/environment.prod.ts
+++ sidm(angular)/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   // serverUrl: 'http://sidm.evalue8.info/',
- serverUrl: 'http://localhost:3000/',
-// serverUrl: 'http://sidm-staging.evalue8.info/',
+//  serverUrl: 'http://localhost:3000/',
+serverUrl: 'http://sidm-staging.evalue8.info/',
 // download: 'http://sidm.evalue8.info/uploads/'
- download: 'http://localhost:3000/uploads/'
-//  download: 'http://sidm-staging.evalue8.info/'
+//  download: 'http://localhost:3000/uploads/'
+ download: 'http://sidm-staging.evalue8.info/'
 
 
 };
