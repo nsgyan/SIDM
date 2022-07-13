9d1e5aed97071b1c7c4d88d6f9102335511e77c7
Shri nivas <shri.nivas@troology.com>
2022-07-13 12:05:43 +0530

    git push

--- sidm(angular)/src/environments/environment.prod.ts
+++ sidm(angular)/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  // serverUrl: 'https://sidm.evalue8.info/',
+  serverUrl: 'https://sidm.evalue8.info/',
   // serverUrl: 'http://localhost:3000/',
-  serverUrl: 'http://sidm-staging.evalue8.info/',
-  // download: 'https://sidm.evalue8.info/uploads/'
+  // serverUrl: 'http://sidm-staging.evalue8.info/',
+  download: 'https://sidm.evalue8.info/uploads/'
   // download: 'http://localhost:3000/uploads/'
-  download: 'http://sidm-staging.evalue8.info/uploads/'
+  // download: 'http://sidm-staging.evalue8.info/uploads/'
 };
